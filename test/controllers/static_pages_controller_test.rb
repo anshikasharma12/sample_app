3cb3cad5cb4a34dc44a94e880fe31b035a95f12d
anshikasharma12 <anshikasharmah@gmail.com>
2024-05-03 13:47:24 +0530

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,10 +2,7 @@ require "test_helper"
 
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get root" do
-      get FILL_IN
-      assert_response FILL_IN
-    end
+  
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
     end

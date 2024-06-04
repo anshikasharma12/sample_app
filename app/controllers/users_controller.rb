c113686aa719e6921bbefdfa4cfa9d636f7de992
anshikasharma12 <anshikasharmah@gmail.com>
2024-06-04 13:38:39 +0530

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
 
@@ -17,20 +17,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    ActiveRecord::Base.transaction do
-      @user = User.new(user_params)
-      if @user.save
-        @user.send_activation_email
-        flash[:info] = "Please check your email to activate your account."
-        redirect_to root_path
-      else
-        render 'new'
-      end
+    @user = User.new(user_params)
+    if @user.save
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account"
+      redirect_to root_url
+    else
+      render 'new'
     end
-  rescue ActiveRecord::StatementInvalid => e
-    Rails.logger.error "Failed to create user: #{e.message}"
-    flash[:danger] = "An error occurred while creating the user. Please try again."
-    render 'new'
   end
 
   def edit
@@ -38,53 +32,66 @@ class UsersController < ApplicationController
   end
 
   def update
-    ActiveRecord::Base.transaction do
-      @user = User.find(params[:id])
-      if @user.update(user_params)
-        flash[:success] = "Profile updated"
-        redirect_to @user
-      else
-        render 'edit'
-      end
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
-  rescue ActiveRecord::StatementInvalid => e
-    Rails.logger.error "Failed to update user: #{e.message}"
-    flash[:danger] = "An error occurred while updating the user. Please try again."
-    render 'edit'
   end
 
   def destroy
-    ActiveRecord::Base.transaction do
-      User.find(params[:id]).destroy
-      flash[:success] = "User deleted"
-      redirect_to users_url
-    end
-  rescue ActiveRecord::StatementInvalid => e
-    Rails.logger.error "Failed to delete user: #{e.message}"
-    flash[:danger] = "An error occurred while deleting the user. Please try again."
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
     redirect_to users_url
   end
 
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-
-  # def logged_in_user
-  #   unless logged_in?
-  #     store_location
-  #     flash[:danger] = "Please log in."
-  #     redirect_to login_url
-  #   end
-  # end
-
-  def correct_user
+  def following
+    @title = "Following"
     @user = User.find(params[:id])
-    redirect_to(root_url) unless current_user?(@user)
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
   end
 
-  def admin_user
-    redirect_to(root_url) unless current_user.admin?
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
+
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
+
+
+
+
+
+
+
+
+
+
+
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -55,7 +55,28 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.authenticated?(:remember, '')
   end
          
-  # test "the truth" do
-  #   assert true
-  # end
+ test "should follow and unfollow a user" do
+  michael = users(:michael)
+  archer = users(:michael)
+  assert_not michael.following?(archer)
+  michael.follow(archer)
+  assert michael.following?(archer)
+  assert archer.followers.include?(michael)
+  michael.unfollow(archer)
+  assert_not michael.following?(archer)
+ end
+ test "feed should have the right posts" do
+  michael = users(:michael)
+  archer = users(:archer)
+  lana = users(:lana)
+  lana.microposts.each do |post_following|
+    assert michael.feed.include?(post_following)
+  end
+  michael.microposts.each do |post_following|
+    assert michael.feed.include?(post_following)
+  end
+  archer.microposts.each do |post_unfollowed|
+    assert_not michael.feed.include?(post_unfollowed)
+  end
+end
 end
